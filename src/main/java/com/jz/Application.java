f74fd286a0662827365a6e2b979c5297f39fca1b
jingzhenghu <854399602@qq.com>
2020-12-10 13:52:30 +0800

    增加一些日志的配置项

--- src/main/java/com/jz/Application.java
+++ src/main/java/com/jz/Application.java
@@ -13,8 +13,10 @@ import org.springframework.cache.annotation.EnableCaching;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //下面语句使得日志输出使用异步处理，减小输出日志对性能的影响
+        System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
 
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/jz/example/multiThread/ReentrantLockTest.java
+++ src/main/java/com/jz/example/multiThread/ReentrantLockTest.java
@@ -1,8 +1,11 @@
 package com.jz.example.multiThread;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+@Slf4j
 public class ReentrantLockTest {
     /**
      * 详细解释ReentrantLock的使用，主要引用了下面两篇博客
@@ -12,66 +15,59 @@ public class ReentrantLockTest {
 
     private Lock lock = new ReentrantLock();
 
-    public void bFuction() {
+    public void test() {
         String tName = Thread.currentThread().getName();
         try {
-            System.out.println(tName + "-开始获取锁..........");
+            log.info(tName + "-开始获取锁..........");
             lock.lockInterruptibly();
 
-            System.out.println(tName + "-获取到锁了！！！！");
-            System.out.println(tName + "-睡觉了，睡个25秒！");
+            log.info(tName + "-获取到锁了！！！！");
+            log.info(tName + "-睡觉了，睡个25秒！");
             Thread.sleep(25000);
-            System.out.println(tName + "-睡醒了，干活！");
+
+            log.info(tName + "-睡醒了，干活！");
             for (int i = 0; i < 5; i++) {
-                System.out.println(tName + "：" + i);
+                log.info(tName + "：" + i);
             }
         } catch (Exception e) {
-            System.out.println(tName+"-我好像被中断了！");
+            log.info(tName+"-我好像被中断了！");
             e.printStackTrace();
         }finally{
             lock.unlock();
-            System.out.println(tName + "-释放了锁");
+            log.info(tName + "-释放了锁");
         }
     }
 
-    //测试lockInterruptibly的使用
-    public void testInterruptibly() throws Exception{
-        ReentrantLockTest bc = new ReentrantLockTest();
-        Thread t0=new Thread(){
+
+    public static void main(String[] args) {
+        ReentrantLockTest vo = new ReentrantLockTest();
+        Thread t0 = new Thread(){
             @Override
             public void run() {
-                bc.bFuction();
+                vo.test();
             }
         };
 
         Thread t1=new Thread(){
             @Override
             public void run() {
-                bc.bFuction();
+                vo.test();
             }
         };
 
         String tName=Thread.currentThread().getName();
-
-        System.out.println(tName+"-启动t0！");
-        t0.start();
-        System.out.println(tName+"-我等个5秒，再启动t1");
-        Thread.sleep(5000);
-        System.out.println(tName+"-启动t1");
-        t1.start();
-
-        System.out.println(tName+"-t1获取不到锁，t0这货睡觉了，没释放，我等个5秒！");
-        Thread.sleep(5000);
-        System.out.println(tName+"-等了5秒了，不等了，把t1中断了！");
-        t1.interrupt();
-
-//        Thread.sleep(Long.MAX_VALUE);
-    }
-
-    public static void main(String[] args) {
-        ReentrantLockTest bc = new ReentrantLockTest();
         try {
-            bc.testInterruptibly();
+            log.info(tName+"-启动t0！");
+            t0.start();
+            log.info(tName+"-我等个5秒，再启动t1");
+            Thread.sleep(5000);
+            log.info(tName+"-启动t1");
+            t1.start();
+
+            log.info(tName+"-t1获取不到锁，t0这货睡觉了，没释放，我等个5秒！");
+            Thread.sleep(5000);
+            log.info(tName+"-等了5秒了，不等了，把t1中断了！");
+            t1.interrupt();
         } catch (Exception e) {
             e.printStackTrace();
         }

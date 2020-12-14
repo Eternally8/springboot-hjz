0474cbfe06f99af38c501d9652d00383694a67b2
jingzhenghu <854399602@qq.com>
2020-12-14 22:00:00 +0800

    nacos负载均衡测试2

--- springCloudAlibaba/nacos-provider2/src/main/java/com/hjz/controller/ProviderController.java
+++ springCloudAlibaba/nacos-provider2/src/main/java/com/hjz/controller/ProviderController.java
@@ -5,6 +5,7 @@ import com.hjz.model.StudentVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/nprovider")
 public class ProviderController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @ApiOperation(value = "提供对外服务",notes = "测试提供对外服务")
     @PostMapping(value = "/myHandle")
     public StudentVo myHandle(@RequestBody StudentVo studentVo){
         log.info("np获取了参数:{}", JSON.toJSONString(studentVo));
 
+        studentVo.setName(serverPort);
         studentVo.setAge(studentVo.getAge() + 1);
         return studentVo;
     }
-
 
 }

54068b5b5925c6a9652091d6cbc9cae66abf3967
jingzhenghu <854399602@qq.com>
2021-02-09 18:47:04 +0800

    增加dubbo结构

---
+++ springCloudAlibaba/dubbo/dubbo-provider/src/main/java/com/hjz/DubboProviderApplication.java
@@ -0,0 +1,17 @@
+package com.hjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class DubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProviderApplication.class, args);
+    }
+
+}

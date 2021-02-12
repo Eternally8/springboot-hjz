eda51dcd80b3021af6733620285130320e2c5cff
jingzhenghu <854399602@qq.com>
2021-02-12 20:20:45 +0800

    增加dubbo结构

--- springCloudAlibaba/dubbo/dubbo-consumer/src/main/java/com/hjz/DubboConsumerApplication.java
+++ springCloudAlibaba/dubbo/dubbo-consumer/src/main/java/com/hjz/DubboConsumerApplication.java
@@ -1,17 +1,27 @@
 package com.hjz;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
-@EnableFeignClients
-@EnableDiscoveryClient
 public class DubboConsumerApplication {
+
+    @DubboReference(version = "${demo.service.version}")
+    private DemoService demoService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(DubboConsumerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> log.info(demoService.sayHello("mercyblitz"));
     }
 
 }

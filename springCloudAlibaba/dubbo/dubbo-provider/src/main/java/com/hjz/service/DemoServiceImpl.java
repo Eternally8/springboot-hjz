5bb71084196c56f9fd4be5a3f9409981b7de27d2
jingzhenghu <854399602@qq.com>
2021-03-09 15:55:13 +0800

    修改了引用依赖和项目结构

--- springCloudAlibaba/dubbo/dubbo-provider/src/main/java/com/hjz/service/DemoServiceImpl.java
+++ springCloudAlibaba/dubbo/dubbo-provider/src/main/java/com/hjz/service/DemoServiceImpl.java
@@ -2,12 +2,14 @@ package com.hjz.service;
 
 import com.jz.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Service;
 
 /**
  * Description： TODO
  * Author: hujingzheng
  * Date: 2021/3/6 16:03
  */
+@Service
 @DubboService(version = "${demo.service.version}")
 public class DemoServiceImpl implements DemoService {
 
